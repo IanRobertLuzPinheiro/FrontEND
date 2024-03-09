ab1253ab6a443055eb667a8d67902a1de7318e12
Ian Robert <iannrobert@hotmail.com>
2024-03-09 00:39:05 -0300

    Atualização P007

--- Semana07_FEB-P007/JReader/src/app/app.module.ts
+++ Semana07_FEB-P007/JReader/src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormularioComponent } from './components/formulario/formulario.component';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FormularioComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
