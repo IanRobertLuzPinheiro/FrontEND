2f90e6f64650031ab28d187419cdcf96e89b4760
Ian Robert <iannrobert@hotmail.com>
2024-03-10 02:02:08 -0300

    P009

--- Semana09_FEB-P009/Rotas/src/app/app-routing.module.ts
+++ Semana09_FEB-P009/Rotas/src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RotaWikiComponent } from './components/rota-wiki/rota-wiki.component';
 import { RotaJreaderComponent } from './components/rota-jreader/rota-jreader.component';
-import { RotaUescAppComponent } from './components/rota-uesc-app/rota-uesc-app.component';
 
 const routes: Routes = [
   { path: 'wiki', component: RotaWikiComponent},
   { path: 'jreader', component: RotaJreaderComponent},
-  { path: 'uesc', component: RotaUescAppComponent}
 ];
 
 @NgModule({
--- Semana09_FEB-P009/Rotas/src/app/app.module.ts
+++ Semana09_FEB-P009/Rotas/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RotaWikiComponent } from './components/rota-wiki/rota-wiki.component';
 import { RotaJreaderComponent } from './components/rota-jreader/rota-jreader.component';
-import { RotaUescAppComponent } from './components/rota-uesc-app/rota-uesc-app.component';
 import { PesquisaComponent } from './components/rota-wiki/components/pesquisa/pesquisa.component';
 import { ResultadoComponent } from './components/rota-wiki/components/resultado/resultado.component';
 import { WikiService } from './components/rota-wiki/components/services/wiki.service';
@@ -16,13 +15,6 @@ import { SecondCategoriaComponent } from './components/rota-jreader/components/s
 import { ThirdCategoriaComponent } from './components/rota-jreader/components/third-categoria/third-categoria.component';
 import { LastCategoriaComponent } from './components/rota-jreader/components/last-categoria/last-categoria.component';
 import { ListagemComponent } from './components/rota-jreader/components/listagem/listagem.component';
-import { DestaquesComponent } from './components/rota-uesc-app/components/destaques/destaques.component';
-import { FooterComponent } from './components/rota-uesc-app/components/footer/footer.component';
-import { HeaderComponent } from './components/rota-uesc-app/components/header/header.component';
-import { MenuComponent } from './components/rota-uesc-app/components/menu/menu.component';
-import { NoticiasComponent } from './components/rota-uesc-app/components/noticias/noticias.component';
-import { ResultadosComponent } from './components/rota-uesc-app/components/resultados/resultados.component';
-import { ServicosComponent } from './components/rota-uesc-app/components/servicos/servicos.component';
 import { BoldSearchPipe } from './components/rota-wiki/components/pipes/pipes.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BuscaComponent } from './components/rota-jreader/components/busca/busca.component';
@@ -32,7 +24,6 @@ import { BuscaComponent } from './components/rota-jreader/components/busca/busca
     AppComponent,
     RotaWikiComponent,
     RotaJreaderComponent,
-    RotaUescAppComponent,
     PesquisaComponent,
     ResultadoComponent,
     CategoriasComponent,
@@ -41,13 +32,6 @@ import { BuscaComponent } from './components/rota-jreader/components/busca/busca
     ThirdCategoriaComponent,
     LastCategoriaComponent,
     ListagemComponent,
-    DestaquesComponent,
-    FooterComponent,
-    HeaderComponent,
-    MenuComponent,
-    NoticiasComponent,
-    ResultadosComponent,
-    ServicosComponent,
     BoldSearchPipe,
     BuscaComponent,
 
